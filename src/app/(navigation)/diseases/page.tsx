9f09df5882599e29d65ec2dacd06e44dc6c68967
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2024-03-16 15:05:21 -0300

    feat: add disease page and disease data

--- src/app/(navigation)/diseases/page.tsx
+++ src/app/(navigation)/diseases/page.tsx
@@ -1,7 +1,21 @@
+import { diseases } from "@/utils/diseases"
+import Link from "next/link"
+
+export interface DiseasesProps {
+    id: string
+    doenca: string
+    sintomas: string
+    tratamento: string
+}
+
 export default function page() {
     return (
-        <div className="w-full px-40 py-10">
-            dasda
+        <div className="w-full px-40 pt-10 grid grid-cols-4 gap-5 pb-20">
+            {diseases.map((disease: DiseasesProps) => (
+                <Link href={`/diseases/${disease.id}`} key={disease.id} className="hover:bg-blue-400 text-center p-2 flex items-center justify-center rounded-md font-semibold hover:text-white">
+                    {disease.doenca}
+                </Link>
+            ))}
         </div>
     )
 }
